0c44b38ae27e204ed71edd238c8b5320d7a7ce00
Islam Seddiq <islamseddiq17@gmail.com>
2025-07-09 18:37:11 +0300

    Update simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,3 @@
-# principal with a value of 1000 (representing $1000),
-# rate with a value of 0.05 (representing 5% annual interest rate),
-# time with a value of 3 (representing 3 years).
 principal = 1000
 rate = 0.05
 time = 3
