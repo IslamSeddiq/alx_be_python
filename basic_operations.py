8870aa10dc58ea82e51be489879d29ea9a81da69
Islam Seddiq <islamseddiq17@gmail.com>
2025-07-09 18:29:47 +0300

    Delete basic_operations.py

--- basic_operations.py
+++
@@ -1,8 +0,0 @@
-number1 = 10
-number2 = 5
-Addition = number1 + number2
-print("Addition of 10 and 5 is", Addition)
-Subtraction = number1 - number2
-print("Subtraction of 10 and 5 is", Subtraction)
-Multiplication = number1 * number2
-print("Multiplication of 10 and 5 is", Multiplication)
