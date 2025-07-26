87adb584fd7460720f7b4b4bfc3d501588eeb4f5
Islam Seddiq <islamseddiq17@gmail.com>
2025-07-26 21:50:01 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,25 @@
-def perform_operation(num1, num2, operation):
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
-    match operation:
-        case "add":
-            print(num1 + num2)
-        case "subtract":
-            print(num1 - num2)
-        case "multiply":
-            print(num1 * num2)
-        case "divide":
-            # handling for division by zero
-            if num2 == 0:
-                print("Cannot be divided by 0")
-            else:
-                print(num1 / num2)
+def perform_operation(num1: float, num2: float, operation: str):
+    """
+    Performs an arithmetic operation on two numbers.
 
-perform_operation()
+    Parameters:
+    - num1 (float): The first number.
+    - num2 (float): The second number.
+    - operation (str): The operation to perform: 'add', 'subtract', 'multiply', 'divide'.
+
+    Returns:
+    - float: Result of the arithmetic operation.
+    - str: Error message if division by zero or invalid operation.
+    """
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 == 0:
+            return "ERROR_DIVISION_BY_ZERO"
+        return num1 / num2
+    else:
+        return "ERROR_INVALID_OPERATION"
